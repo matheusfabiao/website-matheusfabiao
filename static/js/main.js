28486ff9183665295bde7478839d11c91f0a144b
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-07-25 14:41:18 -0300

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,41 @@
+// Typed JS Animation
+// https://github.com/mattboldman/typed.js
+let typed = new Typed('#element', {
+    strings: ['Software.', 'Dados.', 'IA.'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+});
+
+// Navbar Background Change
+const navbar = document.getElementById('navbar');
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
+    
+    if (currentScrollY > 1) {
+        // Se estiver rolando para baixo e não tiver a classe, adiciona
+        if (!navbar.classList.contains('scrolled', 'box-shadow-bottom')) {
+            navbar.classList.add('scrolled', 'box-shadow-bottom');
+        }
+    } else {
+        // Se estiver no topo e tiver a classe, remove
+        if (navbar.classList.contains('scrolled', 'box-shadow-bottom')) {
+            navbar.classList.remove('scrolled', 'box-shadow-bottom');
+        }
+    }
+    
+    lastScrollY = currentScrollY;
+});
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
